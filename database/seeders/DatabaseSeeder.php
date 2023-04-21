41ee3bde058be1e7db025b41d93322d554dd2aec
oyenet1 <bowofadeoyerinde@gmail.com>
2023-04-21 12:34:58 +0100

    ok done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             LaratrustSeeder::class,
+            UserSeeder::class,
             PatientSeeder::class,
         ]);
     }
